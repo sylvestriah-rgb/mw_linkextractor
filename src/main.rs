cb6a0d3f2991bdb5090021ab250cb1d2694cc2b7
BuildTools <unconfigured@null.spigotmc.org>
2025-09-24 14:41:45 -0700

    rust wayback machine archiver from mediawiki. reads urls from wiki using bot account, filters for blacklisted words, submits to archive.org, logs successful and failed url archival attempts. bot credentials and blacklist specified in cargo.toml.

---
+++ src/main.rs
@@ -0,0 +1,37 @@
+use chrono::Utc;
+use toml;
+use serde::Deserialize;
+
+mod linkharvest;
+mod writer;
+mod archiver;
+
+#[derive(Deserialize)]
+struct Config {
+    wiki_api_url: String,
+    bot_username: String,
+    bot_password: String,
+    blacklist: Vec<String>
+}
+
+fn load_config() -> Result<Config,Box<dyn std::error::Error>> {
+    let config_str = std::fs::read_to_string("config.toml")?;
+    let config: Config = toml::from_str(&config_str)?;
+    Ok(config)
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let config = load_config()?;
+    let date = Utc::now().format("%d%m%Y");
+    let links_file = format!("extlinks_{}.txt", date);
+    let success_file = format!("sucesslinks_{}.txt", date);
+    let fail_file = format!("faillinks_{}.txt", date);
+
+    let linklist = linkharvest::run(config.wiki_api_url, config.bot_username, config.bot_password).await?;
+    let filtered_list = writer::exclude_keywords(config.blacklist,linklist);
+
+    writer::write_to_file(filtered_list,links_file.as_str()).await?;
+    archiver::archive_org_submit(links_file.as_str(), success_file.as_str(), fail_file.as_str()).await?;
+    Ok(())
+}
